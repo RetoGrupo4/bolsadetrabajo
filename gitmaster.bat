3dfb830a40c2ade5fb2dae9372eff8e424f24529
RetoGrupo4 <githubtxurdi@gmail.com>
2019-01-08 13:11:58 +0100

    Auto-committed on 08/01/2019

--- gitmaster.bat
+++ gitmaster.bat
@@ -1,14 +1,5 @@
 
 @echo off
-echo type "m = MASTER" or "w = WIP"
-
-:P
-set RAMA=
-set /P RAMA=RAMA: %=%
-
-if "%RAMA%"=="m" (
-
-@echo off
 echo type "s = Subir a github" or "b = bajar de github"
 cd "C:\Users\asaiz\Documents\GitHub\bolsadetrabajo"
 
@@ -20,48 +11,16 @@ set ACTION=
 set /P ACTION=Action: %=%
 if "%ACTION%"=="s" (
 
-%GIT_PATH% init
-%GIT_PATH% checkout master
-  %GIT_PATH% add .
+	
+	%GIT_PATH% checkout master
+  	%GIT_PATH% add .
 	%GIT_PATH% commit -am "Auto-committed on %date%"
 	%GIT_PATH% push origin %BRANCH%
 )
 if "%ACTION%"=="b" (
 %GIT_PATH% checkout master
-%GIT_PATH% init
+
 	%GIT_PATH% pull %BRANCH%
 )
-if "%ACTION%"=="exit" exit /b
-goto P
-)
-
-if "%RAMA%"=="w" (
-
-@echo off
-echo type "s = Subir a github" or "b = bajar de github"
-cd "C:\Users\asaiz\Documents\GitHub\bolsadetrabajo"
-
-set GIT_PATH="C:\Program Files\Git\bin\git.exe"
-set BRANCH = "WIP"
-
-:P
-set ACTION=
-set /P ACTION=Action: %=%
-if "%ACTION%"=="s" (
-%GIT_PATH% checkout WIP
-%GIT_PATH% init
-  %GIT_PATH% add .
-	%GIT_PATH% commit -am "Auto-committed on %date%"
-	%GIT_PATH% push origin %BRANCH%
-)
-if "%ACTION%"=="b" (
-git checkout WIP
-%GIT_PATH% init
-	%GIT_PATH% pull %BRANCH%
-)
-if "%ACTION%"=="exit" exit /b
-goto P
-)
-
 if "%ACTION%"=="exit" exit /b
 goto P
