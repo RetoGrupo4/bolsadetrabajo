68d81d124616ed710b9b04f87c7f26dcdd7b32fc
RetoGrupo4 <githubtxurdi@gmail.com>
2019-01-08 12:41:54 +0100

    Auto-committed on 08/01/2019

---
+++ gitmaster.bat
@@ -0,0 +1,23 @@
+
+@echo off
+echo type "s = Subir a github" or "b = bajar de github"
+cd "C:\Users\asaiz\Documents\GitHub\bolsadetrabajo"
+
+set GIT_PATH="C:\Program Files\Git\bin\git.exe"
+set BRANCH = "master"
+
+:P
+set ACTION=
+set /P ACTION=Action: %=%
+if "%ACTION%"=="s" (
+%GIT_PATH% init
+  %GIT_PATH% add .
+	%GIT_PATH% commit -am "Auto-committed on %date%"
+	%GIT_PATH% push origin %BRANCH%
+)
+if "%ACTION%"=="b" (
+%GIT_PATH% init
+	%GIT_PATH% pull %BRANCH%
+)
+if "%ACTION%"=="exit" exit /b
+goto P
