324a865857a9d4396d6080a61e5e40483f0e3f1c
RetoGrupo4 <githubtxurdi@gmail.com>
2019-01-08 13:59:08 +0100

    Auto-committed on 08/01/2019

---
+++ gitWIP.bat
@@ -0,0 +1,30 @@
+@echo off
+echo type "s = Subir a github" or "b = bajar de github"
+cd "C:\Users\asaiz\Documents\GitHub\bolsadetrabajo"
+
+
+set GIT_PATH="C:\Program Files\Git\bin\git.exe"
+set BRANCH = "master"
+
+
+:P
+set ACTION=
+set /P ACTION=Action: %=%
+if "%ACTION%"=="s" (
+
+
+	%GIT_PATH% checkout %BRANCH%
+	%GIT_PATH% add .
+	%GIT_PATH% commit -am "Auto-committed on %date%"
+	%GIT_PATH% push origin %BRANCH%
+
+
+
+)
+if "%ACTION%"=="b" (
+	
+	%GIT_PATH% pull --tags
+	%GIT_PATH% pull %BRANCH%
+)
+if "%ACTION%"=="exit" exit /b
+goto P
